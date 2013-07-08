2099a41a24dfd0a500ab833c5bb7422677795539
Joseph Jezak josejx@gentoo.org <josejx@gentoo.org>
2013-07-08 01:48:28 -0400

    Update title.

--- conf.lua
+++ conf.lua
@@ -1,10 +1,8 @@
 -- Configure the Love2D environment
 function love.conf(t)
-	t.title = "GET OFF THE PHONE YOU DICK."
+	t.title = "The Way of the Phone Ninja"
 	t.version = "0.8.0"
 	t.screen.width = 1280
 	t.screen.height = 720
---	t.screen.width = 1920
---	t.screen.height = 1080
 end
 
